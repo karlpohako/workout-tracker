d3660405c768f617c4e632ea33fda683e2bae458
Karl Erik Põhako <est.karl@gmail.com>
2025-12-07 15:12:56 +0200

    Initial commit

---
+++ src/main/java/ee/karl/strength_training_tracker/StrengthTrainingTrackerApplication.java
@@ -0,0 +1,13 @@
+package ee.karl.strength_training_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrengthTrainingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrengthTrainingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/karl/strength_training_tracker/StrengthTrainingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.karl.strength_training_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrengthTrainingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
